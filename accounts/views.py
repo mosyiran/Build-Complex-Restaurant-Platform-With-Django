51b5aa2d2fece301d519d19300bc4a041a4250c4
mosyiran <eng.goudarzi@outlook.com>
2025-01-14 17:33:11 +0330

    complete part 3 User Registration, Django messages and errors

--- accounts/views.py
+++ accounts/views.py
@@ -9,10 +9,25 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+
+            # create the user using form
+            # password = form.cleaned_data('password')
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # form.save()
+            # create user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
-            form.save()
-            return redirect('registerUser')
+            user.save()
+        else:
+            print("form invalid")
+            print(form.errors)
     else:
         form = UserForm()
     context = {
